50658a18de8aa3203dbfecd0d241c231a13addec
jser.me <dev.hubo@gmail.com>
2014-03-04 22:14:19 +0800

    Initial Commit

---
+++ chapter6/arcTest/arcTest/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  arcTest
+//
+//  Created by 胡波 on 14-3-4.
+//  Copyright (c) 2014年 jser.me. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
