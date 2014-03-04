68e8ee958bfac18dbc790a09f6a8983bf41e34ed
jser.me <dev.hubo@gmail.com>
2014-03-04 21:56:25 +0800

    Initial Commit

---
+++ chapter4/chapter4/chapter4/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  chapter4
+//
+//  Created by 胡波 on 14-3-4.
+//  Copyright (c) 2014年 胡波. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Objective-C编程练习
+
+代码呀，还是得写才行！
+
+这是[Objective-C编程](http://book.douban.com/subject/19962787/)里的练习题的答案
