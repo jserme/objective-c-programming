28806e275c6c8484ccbacbd7568cd3d9cea0ddd3
jser.me <dev.hubo@gmail.com>
2014-03-04 22:21:36 +0800

    add

--- chapter6/arcTest/arcTest/main.c
+++ chapter6/arcTest/arcTest/main.c
@@ -2,14 +2,17 @@
 //  main.c
 //  arcTest
 //
-//  Created by 胡波 on 14-3-4.
+//  Created by jser.me on 14-3-4.
 //  Copyright (c) 2014年 jser.me. All rights reserved.
 //
 
 #include <stdio.h>
+#include <math.h>
 
 int main(int argc, const char * argv[])
 {
+    
+    printf("1 radian's sin is %f\n", sin(1));
 
     // insert code here...
     printf("Hello, World!\n");
