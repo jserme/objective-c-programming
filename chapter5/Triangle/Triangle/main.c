67bb2bc733482e887df39594632333003e70ff9e
jser.me <dev.hubo@gmail.com>
2014-03-04 22:05:36 +0800

    init

--- chapter5/Triangle/Triangle/main.c
+++ chapter5/Triangle/Triangle/main.c
@@ -2,15 +2,24 @@
 //  main.c
 //  Triangle
 //
-//  Created by 胡波 on 14-3-4.
+//  Created by jser.me on 14-3-4.
 //  Copyright (c) 2014年 jser.me. All rights reserved.
 //
 
 #include <stdio.h>
 
+float remainingAngle(float a, float b){
+    return 180-a-b;
+}
+
 int main(int argc, const char * argv[])
 {
 
+    float angleA = 30.0;
+    float angleB = 60;
+    float angleC = remainingAngle(angleA, angleB);
+    
+    printf("The third angle is %.2f\n", angleC);
     // insert code here...
     printf("Hello, World!\n");
     return 0;
