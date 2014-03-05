72a719ace2059cf2fa99cd8f2bb086ee3821070c
胡伯 <hubo.hb@taobao.com>
2014-03-05 16:37:31 +0800

    Initial Commit

---
+++ chapter8/sizeoffloat/sizeoffloat/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  sizeoffloat
+//
+//  Created by 胡波 on 14-3-5.
+//  Copyright (c) 2014年 jser.me. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
