1fe58180deba82645eb5a4d9658f3cf064d036bd
胡伯 <hubo.hb@taobao.com>
2014-03-10 14:58:52 +0800

    add

--- chapter8/sizeoffloat/sizeoffloat/main.c
+++ chapter8/sizeoffloat/sizeoffloat/main.c
@@ -2,17 +2,19 @@
 //  main.c
 //  sizeoffloat
 //
-//  Created by 胡波 on 14-3-5.
+//  Created by jser.me on 14-3-5.
 //  Copyright (c) 2014年 jser.me. All rights reserved.
 //
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[])
-{
+int main(int argc, const char *argv[]) {
+  int i = 17;
+  int *addressOfI = &i;
+  printf("address is %p\n", addressOfI);
+  printf("read i through address %d \n", *addressOfI);
 
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+  float num = 12.0;
+  printf("float size is %zu\n", sizeof(num));
+  return 0;
 }
-
